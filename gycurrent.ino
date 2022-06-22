5560157dff1922fa204e8606a25de0246a5bb57c
meghanmane84 <79691986+meghanmane84@users.noreply.github.com>
2022-06-22 13:43:36 +0530

    Update gycurrent.ino

--- gycurrent.ino
+++ gycurrent.ino
@@ -28,21 +28,14 @@ void setup() {
       // will pause Zero, Leonardo, etc until serial console opens
     delay(1000);
   }
-
-  uint32_t currentFrequency;
-    
+  uint32_t currentFrequency;  
   Serial.println("Hello!");
-
   if (! ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
     while (1) { delay(10); }
   }
-
   Serial.println("Measuring voltage and current with INA219 ...");
-
-
 }
-
 void loop() {
   Wire.beginTransmission(gyx);
   Wire.write(0x43);
@@ -74,19 +67,15 @@ void loop() {
   float current_mA = 0;
   float loadvoltage = 0;
   float power_mW = 0;
-
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current_mA = ina219.getCurrent_mA();
   power_mW = ina219.getPower_mW();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-  
+  loadvoltage = busvoltage + (shuntvoltage / 1000);  
   Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
   Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
   Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
   Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
   Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.println("");
-
-  
+  Serial.println("");  
 }
